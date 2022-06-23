c7e7ab66959f46d33f6d41f923900042d5226060
George <gnjuki19@gmail.com>
2022-06-23 15:52:45 +0300

    Defined a function to be used to fetct the jokes from an API

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ const button = document.getElementById("button");
 let apiURL = "https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark,Pun,Spooky?type=twopart&amount=10";
 
 
-// function getJokes(){
-//     fetch(apiURL)
-//     .then(function (res){
-//         return res.json();
-//     })
-//     .then(function (data){
-//         quotes.innerHTML = data.jokes[0].setup;
-//         author.innerHTML = data.jokes[0].delivery;
-//         // console.log(data.jokes[0].delivery);
-//     })
-// }
+function getJokes(){
+    // fetch(apiURL)
+    // .then(function (res){
+    //     return res.json();
+    // })
+    // .then(function (data){
+    //     quotes.innerHTML = data.jokes[0].setup;
+    //     author.innerHTML = data.jokes[0].delivery;
+        // console.log(data.jokes[0].delivery);
+    })
+}
 
 // // getJokes();
 
