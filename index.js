5336ae966c025cdd6239108c289c8f1c5697035b
George <gnjuki19@gmail.com>
2022-06-23 17:14:05 +0300

    Made some notes on my index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // alert("Hello there!")
+// Made some variables in reference to the elements in the html
 
 const quotes = document.getElementById("quotes");
 const author = document.getElementById("author");
@@ -6,6 +7,9 @@ const button = document.getElementById("button");
 
 let apiURL = "https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark,Pun,Spooky?type=twopart&amount=10";
 
+// THis function will be used to fetch the data from the above API;
+// It will also render the data in the container div when called inside the 
+// event listener below.
 
 function getJokes(){
     fetch(apiURL)
@@ -20,6 +24,26 @@ function getJokes(){
 }
 
 // getJokes();
+
+
+// This function will be used to generate background colors at random on click events
+// function randomize() {
+//     var rgbColor = aqua;
+//     red = Math.floor(Math.random() * 250 + 0);
+//     green = Math.floor(Math.random() * 250 + 0);
+//     blue = Math.floor(Math.random() * 250 + 0);
+  
+//     rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+//     document.getElementById("container").style.background = rgbColor;
+  
+//     red = ("0" + red.toString(16)).substr(-2).toUpperCase();
+//     green = ("0" + green.toString(16)).substr(-2).toUpperCase();
+//     blue = ("0" + blue.toString(16)).substr(-2).toUpperCase();
+//   }
+  
+  // randomize();
+
+
 
 
 button.addEventListener("click", getJokes);
